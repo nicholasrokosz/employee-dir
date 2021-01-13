d36319850215935a456b9ad3440c2a9567d2739c
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-12 17:23:18 -0700

    add react-bootstrap table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import './App.css';
-import Table from './components/Table';
+import Table from 'react-bootstrap/Table';
 
-function App() {
-  return <Table />;
+class App extends React.Component {
+  state = {
+    employees: [],
+  };
+
+  async componentDidMount() {
+    const data = await fetch('https://randomuser.me/api/?results=200&nat=us');
+    const { results: employees } = await data.json();
+    this.setState({ employees });
+  }
+
+  render() {
+    const { employees } = this.state;
+    return (
+      <>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan='2'>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </>
+    );
+  }
 }
 
 export default App;
